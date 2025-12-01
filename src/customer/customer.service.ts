7d642215e27dec3df12e6dfe12d0c76f0f49d2bb
ivan <business.ivan.agustin@gmail.com>
2025-12-01 18:03:00 -0300

    feat: work in progress - customer endpoint

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -5,6 +5,10 @@ import { CustomerRepository } from "./customer.repository";
 export class CustomerService {
   constructor(private customerRepository: CustomerRepository) { }
 
+  async findByEmail(email: string): Promise<Customer | null> {
+    return this.customerRepository.find(email);
+  }
+
   async createCustomer(data: CreateCustomerDTO): Promise<Customer> {
     const newCustomer = this.customerRepository.insert({
       email: data.email,
