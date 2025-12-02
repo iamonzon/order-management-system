2b6a25f920f1bad84eac4ef22edac19e79bb8dbc
imonzon <9726654+iamonzon@users.noreply.github.com>
2025-12-02 18:17:26 -0300

    feat: graphql implementation (#1)
    * chore: add graphql and yoga dependency
    
    * feat: update graphql endpoint for all entities

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,14 @@ import { Router } from "express";
 import { Container } from "../core/container";
 import { createV1Routes } from "./v1";
 import { createHealthRoutes } from "./health";
+import { createGraphQLRoutes } from "./graphql";
 
 export function createRoutes(container: Container) {
-  const router = Router()
+  const router = Router();
 
-  // Default
-  router.use('/health', createHealthRoutes(container))
-
-  router.use('/v1', createV1Routes(container))
+  router.use("/health", createHealthRoutes(container));
+  router.use("/v1", createV1Routes(container));
+  router.use("/graphql", createGraphQLRoutes(container));
 
   return router;
 }
